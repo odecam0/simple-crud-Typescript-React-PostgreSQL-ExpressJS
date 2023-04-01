8ed5ceaa35a845b3a44409a476b90f6ca15aee63
Bruno Macedo <macedobruno@id.uff.br>
2023-04-01 19:35:55 -0300

    Testando DB_STRING

--- db.mjs
+++ db.mjs
@@ -1,6 +1,7 @@
 import postgres from 'postgres';
 // postgres = require('postgres');
 
-export const sql = postgres("postgresql://" + process.env.DB_USER + ":" + process.env.DB_PSSWD + "@" + process.env.DB_NET + ":" + process.env.DB_PORT + "/" + process.env.DB_NAME);
+// export const sql = postgres("postgresql://" + process.env.DB_USER + ":" + process.env.DB_PSSWD + "@" + process.env.DB_NET + ":" + process.env.DB_PORT + "/" + process.env.DB_NAME);
+export const sql = postgres(process.env.DB_STRING);
 
 // export default sql;
